105201175a41daf324fef52722cf38e5a4f21830
Jeevan Pingali <jeevan.pingali@gmail.com>
2020-12-26 12:06:10 +0530

    So far

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -1,14 +1,18 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object ActorCapabilities extends App {
   class SimpleActor extends Actor {
     override def receive: Receive = {
+      case "Hi" => sender() ! "Hello, there!"
       case message: String => println(s"[$self] I have received $message")
       case number: Int => println(s"I have received a number: $number")
       case SpecialMessage(contents) => println(s"[simple actor] I have received something special: ${contents}")
-      case SendMessageToYourself(content) => self ! content
+      case SendMessageToYourself(content) =>
+        self ! content
+      case SayHiTo(ref) => ref ! "Hi"
+      case WirelessPhoneMessage(content, ref) => ref forward (content + "s")
     }
   }
 
@@ -32,6 +36,21 @@ object ActorCapabilities extends App {
   case class SendMessageToYourself(content: String)
   simpleActor ! SendMessageToYourself("I am an actor and I'm proud of it")
 
+  // 3 - actors can reply to messages
+  val alice = system.actorOf(Props[SimpleActor], "alice")
+  val bob = system.actorOf(Props[SimpleActor], "bob")
+
+  case class SayHiTo(ref: ActorRef)
+
+  alice ! SayHiTo(bob)
+
+  // 4 - dead letters
+  alice ! "Hi"
+
+  // 5 - forwarding messages
+  case class WirelessPhoneMessage(content: String, ref: ActorRef)
+
+  alice ! WirelessPhoneMessage("Hi", bob)
 
   system.terminate()
 }
