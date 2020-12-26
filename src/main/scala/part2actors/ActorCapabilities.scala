0ce9575f4508f63310e04f7df6b2b11d2c722a44
Jeevan Pingali <jeevan.pingali@gmail.com>
2020-12-26 11:48:37 +0530

    So far

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -5,14 +5,15 @@ import akka.actor.{Actor, ActorSystem, Props}
 object ActorCapabilities extends App {
   class SimpleActor extends Actor {
     override def receive: Receive = {
-      case message: String => println(s"[simple actor] I have received $message")
+      case message: String => println(s"[$self] I have received $message")
       case number: Int => println(s"I have received a number: $number")
       case SpecialMessage(contents) => println(s"[simple actor] I have received something special: ${contents}")
+      case SendMessageToYourself(content) => self ! content
     }
   }
 
   val system = ActorSystem("ActorCapabilitiesDemo")
-  val simpleActor = system.actorOf(Props[SimpleActor])
+  val simpleActor = system.actorOf(Props[SimpleActor], "simpleActor")
 
   simpleActor ! "hello, actor"
 
@@ -26,6 +27,11 @@ object ActorCapabilities extends App {
   simpleActor ! SpecialMessage("Some special content")
 
   // 2 - actors have information about their context and about themselves
+  // context.self === "this" in OOP
+
+  case class SendMessageToYourself(content: String)
+  simpleActor ! SendMessageToYourself("I am an actor and I'm proud of it")
+
 
   system.terminate()
 }
