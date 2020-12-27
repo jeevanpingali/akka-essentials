29ec2bdbcee70bb524ca2949368b6db01b113fa7
Jeevan Pingali <jeevan.pingali@gmail.com>
2020-12-27 16:54:30 +0530

    So far

--- src/main/scala/part2actors/ChangingActorBehaviorExercise.scala
+++ src/main/scala/part2actors/ChangingActorBehaviorExercise.scala
@@ -1,6 +1,6 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import part2actors.ChangingActorBehaviorExercise.Counter.{Decrement, Increment, Print}
 
 object ChangingActorBehaviorExercise extends App {
@@ -30,6 +30,46 @@ object ChangingActorBehaviorExercise extends App {
   counter ! Print
   (1 to 3).foreach(_ => counter ! Decrement)
   counter ! Print
+
+  /**
+   * Exercise 2
+   */
+
+  case class Vote(candidate: String)
+  case object VoteStatusRequest
+  case class VoteStatus(candidate: Option[String])
+  case class VoteStatusReply(candidate: Option[String])
+  class Citizen extends Actor {
+    override def receive: Receive = {
+      case Vote(candidate) => context.become((voted(candidate))
+    }
+
+    def voted(candidate: String): Receive = {
+      case VoteStatusRequest =>sender() !VoteStatusReply(Some(candidate))
+    }
+  }
+  case class AggregateVotes(citizens: Set[ActorRef])
+  class VoteAggregator extends Actor {
+    override def receive: Receive = ???
+  }
+
+  val alice = system.actorOf(Props[Citizen], "alice")
+  val bob = system.actorOf(Props[Citizen], "bob")
+  val charlie = system.actorOf(Props[Citizen], "charlie")
+  val daniel = system.actorOf(Props[Citizen], "daniel")
+
+  alice ! Vote("Martin")
+  bob ! Vote("Jonas")
+  charlie ! Vote("Ronald")
+  daniel ! Vote("Ronald")
+  val voteAggregator = system.actorOf(Props[VoteAggregator])
+  voteAggregator ! AggregateVotes(Set(alice, bob, charlie, daniel))
+
+  /**
+   * Print the status of votes, a map of candidates and the number of votes received
+   */
+
+
 
   system.terminate()
 
