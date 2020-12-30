8f83f47f591a926fd2c689c7b88c3592bb26a975
Jeevan Pingali <jeevan.pingali@gmail.com>
2020-12-30 22:00:41 +0530

    Interim commit

--- src/main/scala/part2actors/ChildActorsExercise.scala
+++ src/main/scala/part2actors/ChildActorsExercise.scala
@@ -1,44 +1,76 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object ChildActorsExercise extends App {
 
   // Distributed word counting
   object WordCounterMaster {
     case class Initialize(nChildren: Int)
-    case class WordCountTask(text: String)
-    case class WordCountReply(count: Int)
+    case class WordCountTask(id: Int, text: String)
+    case class WordCountReply(id: Int, count: Int)
   }
   class WordCounterMaster extends Actor {
     import WordCounterMaster._
     var lastCount = 0
     override def receive: Receive = {
       case Initialize(workerCount) =>
+        println("workerCount: " + workerCount)
         println(s"${self.path} initializing with count: $workerCount")
         for(n <- 0 until workerCount) yield
           context.actorOf(Props[WordCounterWorker], s"worker$n")
-        context.become(wordCountTaskExecution(workerCount, 0))
+        context.become(wordCountTaskExecution(workerCount, 0, 0, Map()))
     }
 
-    def wordCountTaskExecution(workerCount: Int, workerIndex: Int): Receive = {
-      case text: String =>
+    def wordCountTaskExecution(workerCount: Int, workerIndex: Int, currentTaskId: Int, requestMap: Map[Int, ActorRef]): Receive = {
+      case Initialize(workerCount) =>
+        println("wrong place")
+      case text =>
+        val originalSender = sender()
         val index = if(workerCount == workerIndex) 0 else workerIndex
         val workerRef = context.actorSelection(s"/user/master/worker$index")
-        context.become(wordCountTaskExecution(workerCount, index + 1))
-        workerRef ! WordCountTask(text)
-      case WordCountReply(count) =>
+        workerRef ! WordCountTask(currentTaskId, text.toString)
+        val newRequestMap = requestMap + (currentTaskId -> originalSender)
+        context.become(wordCountTaskExecution(workerCount, index + 1, currentTaskId + 1, newRequestMap))
+      case WordCountReply(currentTaskId, count) =>
         println(s"${self.path} words counted: $count")
+        val originalSender = requestMap.get(currentTaskId)
+//        originalSender ! count
+        context.become(wordCountTaskExecution(workerCount, workerIndex, currentTaskId, requestMap - currentTaskId))
     }
   }
 
   class WordCounterWorker extends Actor {
     import WordCounterMaster._
     override def receive: Receive = {
-      case WordCountTask(text) =>
+      case WordCountTask(currentTaskId, text) =>
         val wordCount = text.split(" ").length
         println(s"${self.path} words of $text counted: $wordCount")
-        sender() ! WordCountReply(wordCount)
+        sender() ! WordCountReply(currentTaskId, wordCount)
+    }
+  }
+
+  class TestActor extends Actor {
+    import WordCounterMaster._
+
+    override def receive: Receive = {
+      case "go" =>
+        println("[test actor] got to go")
+        val master = system.actorOf(Props[WordCounterMaster], "master2")
+        println(s"[test actor] got master reference is: ${master.path}")
+        master ! Initialize(3)
+        Thread.sleep(1000 * 5)
+        println("Initialization completed")
+
+        val texts = List("I love Akka", "Scala is super done", "yes", "me too")
+/*        texts.foreach(text => {
+          println(s"[test actor] ")
+          master ! text
+          Thread.sleep(500)
+        })*/
+        master ! "Akka is cool"
+      case count =>
+        println(s"[test actor I received a reply: $count")
     }
   }
 
@@ -53,13 +85,19 @@ object ChildActorsExercise extends App {
    */
 
   val system = ActorSystem("ChildActorsExercise")
-  val master = system.actorOf(Props[WordCounterMaster], "master")
+  /*val master = system.actorOf(Props[WordCounterMaster], "master")
   import WordCounterMaster._
-  master ! Initialize(5)
-  (0 to 1000).foreach(_ => {
+  master ! Initialize(3)
+  (0 to 10).foreach(_ => {
     master ! "akka is awesome for sure"
     Thread.sleep(5)
-  })
+  })*/
+
+  val testActor = system.actorOf(Props[TestActor], "testactor")
+  Thread.sleep(5)
+  testActor ! "go"
+
+  Thread.sleep(5 * 1000)
 
   system.terminate()
 }
