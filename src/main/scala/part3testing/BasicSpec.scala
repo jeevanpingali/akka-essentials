bacf9ee55996398c0fb20293a82ab707e699c6c5
Jeevan Pingali <jeevan.pingali@gmail.com>
2020-12-31 08:25:20 +0530

    expect and do not expect a message

--- src/main/scala/part3testing/BasicSpec.scala
+++ src/main/scala/part3testing/BasicSpec.scala
@@ -1,8 +1,10 @@
 package part3testing
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import part3testing.BasicSpec.{BlackHole, SimpleActor}
+import scala.concurrent.duration._
 
 class BasicSpec extends TestKit(ActorSystem("basicspec"))
   with ImplicitSender
@@ -13,15 +15,37 @@ class BasicSpec extends TestKit(ActorSystem("basicspec"))
     TestKit.shutdownActorSystem(system)
   }
 
-  "The thing being tested" should {
-    "do this" in {
+  "TA simple actor" should {
+    "send back the same message" in {
       // testing scenario
+      val echoActor = system.actorOf(Props[SimpleActor])
+      val message = "hello, test"
+      echoActor ! message
 
+      expectMsg(message) // akka.test.single-expect-default
+    }
+
+    "a black hole actor" should {
+      "send back some message" in {
+        val blackHoleActor = system.actorOf(Props[BlackHole])
+        val message = "hello, test"
+        blackHoleActor ! message
+
+        expectNoMessage(1 second)
+      }
     }
 
   }
 }
 
 object BasicSpec {
+  class SimpleActor extends Actor {
+    override def receive: Receive = {
+      case message => sender() ! message
+    }
+  }
 
+  class BlackHole extends Actor {
+    override def receive: Receive = Actor.emptyBehavior
+  }
 }
